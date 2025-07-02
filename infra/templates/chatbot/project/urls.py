7cd6391cc6e9af7492ecb6d81794618e50fd2606
Alexander Fedotov <afedotov89@gmail.com>
2025-07-02 15:47:46 +0300

    Update chatbot requirements and add assistant URL routing
    - Changed the dependency version for the django-assistant-bot in requirements.txt to a more recent commit.
    - Added a new URL pattern in urls.py to include the assistant's URLs, enhancing the routing structure of the chatbot application.

--- infra/templates/chatbot/project/urls.py
+++ infra/templates/chatbot/project/urls.py
@@ -7,9 +7,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+import assistant.assistant.urls
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apps.bot.urls')),
+    path('', include('assistant.assistant.urls')),
 ]
 
 # Serve media files in development
